43f1e8fa49de17096954cfceccc2a658663ca5bd
abdellatif Elsheikh <elshikh532@gmail.com>
2022-03-13 16:33:54 +0200

    add middlewares for login and security

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
 const PORT = 5000;
 
 const app: Application = express();
+// add middleware to parse incoming requests
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
+// add http request logger middleware
+app.use(morgan('common'));
+// http security middleware
+app.use(helmet());
+// create limit for api requests
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'to many requests please try agin after two mints',
 });
-
-app.get('/', (req: Request, res: Response) => {
+app.use(limiter);
+// add get request
+app.get('/', (_req: Request, res: Response) => {
   res.json({
     message: 'Hello world!',
   });
+});
+// add post route
+app.post('/', (req: Request, res: Response) => {
+  req.body = {
+    hamda: 'mosh hamada',
+  };
+  res.json({
+    message: 'hello world from post request',
+    data: req.body,
+  });
+});
+
+// -------------------------------------------------
+// -------------------------------------------------
+// start server---------------------------------------
+app.listen(PORT, () => {
+  console.log(`server is running at port: ${PORT}`);
 });
 
 export default app;
