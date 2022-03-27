b724e0e2e8c507e0faf34a288f7e4dcb958768bf
abdellatif-elsheikh <elshikh532@gmail.com>
2022-03-27 15:36:36 +0200

    handling errors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, request, Request, response, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import config from './config';
+import errorMiddleware from './middlewares/error.middleware';
 
-const PORT = 5000;
+const PORT = config.port || 3000;
 
 const app: Application = express();
 // add middleware to parse incoming requests
 app.use(express.json());
+// security
 
 // add http request logger middleware
 app.use(morgan('common'));
 // http security middleware
 app.use(helmet());
+// error handler middleware
 // create limit for api requests
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
@@ -22,8 +26,12 @@ const limiter = rateLimit({
   message: 'to many requests please try agin after two mints',
 });
 app.use(limiter);
+
+// ----------------------------------------------------------
+// ------------------------ ROUTS ---------------------------
 // add get request
 app.get('/', (_req: Request, res: Response) => {
+  throw new Error('error exist')
   res.json({
     message: 'Hello world!',
   });
@@ -38,6 +46,13 @@ app.post('/', (req: Request, res: Response) => {
     data: req.body,
   });
 });
+
+app.use(errorMiddleware)
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    message: 'you are lost you can go back to Home http://localhost:5000/'
+  })
+})
 
 // -------------------------------------------------
 // -------------------------------------------------
